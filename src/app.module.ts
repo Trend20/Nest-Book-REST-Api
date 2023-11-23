fc87a6b33f935d2d468ee18d855f6c18fba1264a
Trend20 <enockomondi305@gmail.com>
2023-11-23 15:48:53 +0300

    project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BooksService } from './books/books.service';
+import { BooksController } from './books/books.controller';
+import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, BooksModule, AuthModule],
+  providers: [BooksService],
+  controllers: [BooksController],
 })
 export class AppModule {}
